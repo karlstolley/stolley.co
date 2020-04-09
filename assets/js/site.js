bfca1eb927abe58b3eb3a800a31bcfe3d2e31c58
Karl Stolley <karl.stolley@gmail.com>
2020-04-09 14:56:25 -0500

    Officially wire up the ServiceWorker

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,3 +1,14 @@
+// Register service worker, if browser supports them
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js')
+  .then(function(registration) {
+    console.log('Registered service worker scoped to', registration.scope);
+  })
+  .catch(function(error) {
+    console.error('Failed to register service worker', error)
+  });
+}
+
 if (typeof fetch !== 'undefined' && typeof document.querySelector !== 'undefined') {
 
   // Used in the formatted fetch results from GitHub
